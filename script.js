6d3e598d5a5e228608500894360d3298805cc553
Will Dwelle <willdwelle@frontier.com>
2021-10-22 16:31:40 -0700

    Part 2

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function numIncrease(){
     element.innerText = num +" like(s)";
     num++;
 }
+
+var num = 1;
+function numIncrease2(){
+    var element = document.querySelector("#like_count2");
+    element.innerText = num +" like(s)";
+    num++;
+}
+
+var num = 1;
+function numIncrease3(){
+    var element = document.querySelector("#like_count3");
+    element.innerText = num +" like(s)";
+    num++;
+}
